a11786cb56ceb7b0f6b69bad6f4ea069bb7d7d21
iyuvalk <iyuvalk@gmail.com>
2018-02-15 15:40:14 +0200

    Create sampler.py

---
+++ sampler.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python3.5
+import re
+import sys
+import datetime
+import json
+from elasticsearch import Elasticsearch
+from bottle import route, run, template, get, request
+import os
+
+#TODO: Add code for determining the metric name based on the config file + replacements
+#TODO: Add code for updating the metrics via statsd
+
+
+
+elasticsearch_server = "127.0.0.1"
+
+class QueryNameNotFoundOrOfWrongType(Exception):
+    pass
+
+class SmartOnionSampler:
+     queries = {}
+     es = Elasticsearch(hosts=[elasticsearch_server], timeout=30)
+
+     def __init__(self):
+        pass
+
+     def run(self, listen_ip, listen_port, config_file):
+         with open(config_file, 'r') as config_file_obj:
+             SmartOnionSampler.queries = json.load(config_file_obj)
+         run(host=listen_ip, port=listen_port, server="gunicorn", workers=32)
+
+     @get('/smart-onion/field-query/<queryname>')
+     def fieldQuery(queryname):
+         query_details = SmartOnionSampler.queries[queryname]
+         if query_details["type"] != "FIELD_QUERY":
+             raise QueryNameNotFoundOrOfWrongType()
+
+         now = datetime.datetime.now()
+         yesterday = datetime.date.today() - datetime.timedelta(1)
+         time_range = (now.utcnow() - datetime.timedelta(seconds=query_details["time_range"])).isoformat() + " TO " + now.utcnow().isoformat()
+         metric_name = query_details["metric_name"].replace("%{today}", now.strftime("%Y.%m.%d")).replace("%{yesterday}", yesterday.strftime("%Y.%m.%d"))
+         metric_name = metric_name.replace("{{#query_name}}", queryname)
+         query_index = query_details["index_pattern"].replace("%{today}", now.strftime("%Y.%m.%d")).replace("%{yesterday}", yesterday.strftime("%Y.%m.%d"))
+         query_base = query_details["query"].replace("{{#query_name}}", queryname)
+         for i in range(1, 10):
+             if "{{#arg" + str(i) + "}}" in str(query_base):
+                 arg = request.query["arg" + str(i)]
+                 if len(str(arg).strip()) != 0:
+                     query_base = query_base.replace("{{#arg" + str(i) + "}}", arg)
+                     metric_name = metric_name.replace("{{#arg" + str(i) + "}}", arg)
+             else:
+                 break
+
+
+         query_string = "(" + query_base + ") AND @timestamp:[" + time_range + "]"
+         query_body = {
+                     "query":{
+                         "query_string": {
+                             "query": query_string
+                         }
+                     }
+                 }
+
+         try:
+             res = SmartOnionSampler().es.search(
+                 index=query_index,
+                 body=query_body,
+                 size=1
+             )
+             res = "@@RES: " + res['hits']['hits'][0]['_source'][query_details["field_name"]]
+         except Exception as e:
+             res = "@@RES: @@EXCEPTION: " + str(e)
+
+         return res
+
+     @get('/smart-onion/query-count/<queryname>')
+     def queryCount(queryname):
+         query_details = SmartOnionSampler.queries[queryname]
+         if query_details["type"] != "QUERY_COUNT":
+             raise QueryNameNotFoundOrOfWrongType()
+
+         now = datetime.datetime.now()
+         yesterday = datetime.date.today() - datetime.timedelta(1)
+         time_range = (now.utcnow() - datetime.timedelta(seconds=query_details["time_range"])).isoformat() + " TO " + now.utcnow().isoformat()
+         query_index = query_details["index_pattern"].replace("%{today}", now.strftime("%Y.%m.%d")).replace("%{yesterday}", yesterday.strftime("%Y.%m.%d"))
+         query_base = query_details["query"].replace("{{#query_name}}", queryname)
+         for i in range(1, 10):
+             if "{{#arg" + str(i) + "}}" in str(query_base):
+                 arg = request.query["arg" + str(i)]
+                 if len(str(arg).strip()) != 0:
+                     query_base = query_base.replace("{{#arg" + str(i) + "}}", arg)
+             else:
+                 break
+
+
+         query_string = "(" + query_base + ") AND @timestamp:[" + time_range + "]"
+         query_body = {
+                     "query":{
+                         "query_string": {
+                             "query": query_string
+                         }
+                     }
+                 }
+
+         try:
+             res = SmartOnionSampler().es.count(
+                 index=query_index,
+                 body=query_body
+             )
+             res = "@@RES: " + str(res['count'])
+         except Exception as e:
+             res = "@@RES: @@EXCEPTION: " + str(e)
+
+         return res
+
+     @get('/smart-onion/dump_queries')
+     def dump_queries():
+         return "@@RES: " + json.dumps(SmartOnionSampler.queries)
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+listen_ip = "127.0.0.1"
+listen_port = 8080
+config_file_name = "queries.conf"
+config_file_default_path = "/etc/smart-onion/"
+config_file = os.path.join(config_file_default_path, config_file_name)
+config_file_specified_on_cmd = False
+if len(sys.argv) > 1:
+    for arg in sys.argv:
+        if "=" in arg:
+            arg_name = arg.split("=")[0]
+            arg_value = arg.split("=")[1]
+
+            if arg_name == "--listen-ip":
+                if not re.match("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+", arg_value):
+                    print("EEROR: The --listen-ip must be a valid IPv4 address.  Using default of " + str(listen_ip) + " (hardcoded)")
+                else:
+                    listen_ip = arg_value
+
+            if arg_name == "--listen-port":
+                try:
+                    listen_port = int(arg_value)
+                except:
+                    print("ERROR: The --listen-port argument must be numeric. Using default of " + str(listen_port) + " (hardcoded)")
+
+            if arg_name == "--config-file":
+                config_file = arg_value
+                config_file_specified_on_cmd = True
+        else:
+            if arg == "--help" or arg == "-h" or arg == "/h" or arg == "/?":
+                print("USAGE: " + os.path.basename(os.path.realpath(__file__)) + " [--listen-ip=127.0.0.1 --listen-port=8080 --config-file=/etc/smart-onion/queries.conf]")
+                print("")
+                print("-h, --help, /h and /q will print this help screen.")
+                print("")
+                quit(1)
+
+if not os.path.isfile(config_file):
+    if config_file_specified_on_cmd:
+        print("ERROR: The file specified as the config file does not exist.")
+        quit(9)
+    else:
+        config_file = os.path.join(script_path, config_file_name)
+
+if not os.path.isfile(config_file):
+    print ("ERROR: Failed to find the config file in both " + config_file_default_path + " and " + script_path + " (script location)")
+    quit(9)
+
+sys.argv = [sys.argv[0]]
+SmartOnionSampler().run(listen_ip=listen_ip, listen_port=listen_port, config_file=config_file)
