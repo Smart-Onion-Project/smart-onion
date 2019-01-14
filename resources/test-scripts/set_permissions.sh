1246a1bf183275fb231fe3ed93902871f79befe7
yuval <iyuvalk@gmail.com>
2019-01-14 19:15:15 +0200

    set_permissions.sh - Added to set the correct execute permissions on all the services

---
+++ resources/test-scripts/set_permissions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chmod +x /opt/smart-onion/code/backend/python2_7/services/metrics_analyzer/metrics_analyzer.py
+chmod +x /opt/smart-onion/code/backend/python2_7/services/configurator/configurator.py
+chmod +x /opt/smart-onion/code/backend/python2_7/services/alerter/alerter.py
+chmod +x /opt/smart-onion/code/backend/python3_5/services/anomaly_detector/anomaly_detector.py
+chmod +x /opt/smart-onion/code/backend/python3_5/services/metrics_collector/metrics_collector.py
