f3907ca433234769e1ccfaf0d7d7ea1b0ad5a117
yuval <iyuvalk@gmail.com>
2019-03-04 21:02:42 +0200

    tinyurl.py: - Switched to "paste" instead of gunicorn

--- code/backend/python3_5/services/tinyurl/tinyurl.py
+++ code/backend/python3_5/services/tinyurl/tinyurl.py
@@ -119,7 +119,7 @@ class TinyUrl:
         if DEBUG:
             self._app.run(host=self._host, port=self._port)
         else:
-            self._app.run(host=self._host, port=self._port, server="gunicorn", workers=32, timeout=120)
+            self._app.run(host=self._host, port=self._port, server="paste", timeout=120)
 
         self._run_backup_loop = False
 
