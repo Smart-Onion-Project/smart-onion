69804867e241b52c11ede73833622df36c658b4a
yuval <iyuvalk@gmail.com>
2019-04-01 17:24:31 +0300

    queries.json:  - Fixed an issue that sometimes could use the doc_count that has been returned from elastic as the argument for the query even when it wasn't necessary.

--- code/backend/python3_5/services/metrics_collector/metrics_collector.py
+++ code/backend/python3_5/services/metrics_collector/metrics_collector.py
@@ -123,9 +123,9 @@ class Utils:
                         cur_url = cur_url + config_copy["smart-onion.config.architecture.internal_services.backend.metrics-collector.published-listening-host"] + ":"
                     if config_copy is not None and "smart-onion.config.architecture.internal_services.backend.metrics-collector.published-listening-port" in config_copy:
                         cur_url = cur_url + str(config_copy["smart-onion.config.architecture.internal_services.backend.metrics-collector.published-listening-port"])
-                    cur_url = cur_url + services_urls["smart-onion.config.architecture.internal_services.backend.metrics-collector.base_urls." + query_type.lower()] + query_id + "?arg1=" + urllib.parse.quote_plus(str(item[list(item.keys())[0]]))
+                    cur_url = cur_url + services_urls["smart-onion.config.architecture.internal_services.backend.metrics-collector.base_urls." + query_type.lower()] + query_id
 
-                    arg_no = 2
+                    arg_no = 1
                     for key_idx in range(1, len(item.keys())):
                         if list(item.keys())[key_idx] != "{#_DOC_COUNT}":
                             cur_url = cur_url + "&arg" + str(arg_no) + "=" + urllib.parse.quote_plus(str(item[list(item.keys())[key_idx]]))
