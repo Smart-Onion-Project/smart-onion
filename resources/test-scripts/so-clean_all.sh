d1558073e0055a58ff1c6c7d1c2c037fd490890a
yuval <iyuvalk@gmail.com>
2019-04-01 09:49:58 +0300

    anomaly_detector:  - Added information about the anomalies reported in the logs and in the /ping result
    so-clean_all:
     - Added informational messages
    
    so-stat:
     - Added script for testing all the system services status

--- resources/test-scripts/so-clean_all.sh
+++ resources/test-scripts/so-clean_all.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+echo "Stopping services..."
 systemctl daemon-reload
 /opt/smart-onion/resources/test-scripts/test_services_status.sh stop
+echo "Starting Zookeeper..."
 systemctl start zookeeper
 sleep 2
+echo "Starting Kafka..."
 systemctl start kafka-server
 sleep 2
+echo "Starting StatsD..."
+systemctl start statsd
+sleep 2
+echo "Deleting all metrics in StatsD..."
 for g in `echo "gauges" | nc 127.0.0.1 8126 | sed 's/END//g' | sed "s/'/\"/g" | jq -r "keys[]"`; do echo "delgauges $g" | nc 127.0.0.1 8126; done
+echo "Deleting all relevant topics in Kafka..."
 /opt/kafka_2.11-2.1.0/bin/kafka-topics.sh --zookeeper localhost:2181 --topic metrics --delete
 /opt/kafka_2.11-2.1.0/bin/kafka-topics.sh --zookeeper localhost:2181 --topic metric-collection-tasks --delete
 /opt/kafka_2.11-2.1.0/bin/kafka-topics.sh --zookeeper localhost:2181 --topic metric-analyzer-detected-anomalies --delete
 sleep 5
+echo "Removing all Nupic models..."
 rm -rf /data/models/models/*
+echo "Removing all Nupic anomaly likelihood calculators..."
 rm -rf /data/models/anomaly_likelihood_calcs/*
+echo "Removing all whisper files..."
 rm -rf /data/metrics/whisper/*
 
 # TODO: Regenerate all passwords (for DBs, users, etc)
