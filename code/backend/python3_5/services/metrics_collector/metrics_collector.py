76c727f6e7848f9771261e8554f6111b8346d186
yuval <iyuvalk@gmail.com>
2019-04-01 17:52:17 +0300

    queries.json:  - Whooopsie... (-:

--- code/backend/python3_5/services/metrics_collector/metrics_collector.py
+++ code/backend/python3_5/services/metrics_collector/metrics_collector.py
@@ -128,7 +128,12 @@ class Utils:
                     arg_no = 1
                     for key_idx in range(1, len(item.keys())):
                         if list(item.keys())[key_idx] != "{#_DOC_COUNT}":
-                            cur_url = cur_url + "&arg" + str(arg_no) + "=" + urllib.parse.quote_plus(str(item[list(item.keys())[key_idx]]))
+                            if arg_no == 1:
+                                cur_url = cur_url + "?"
+                            else:
+                                cur_url = cur_url + "&"
+
+                            cur_url = cur_url + "arg" + str(arg_no) + "=" + urllib.parse.quote_plus(str(item[list(item.keys())[key_idx]]))
                             arg_no = arg_no + 1
 
                     # Translate the url to a tiny url
